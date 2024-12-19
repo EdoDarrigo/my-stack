3dbac1616efd048da6b7a77932b76942bd5d2516
edarr <edarr@LAPTOP-F3ML6517>
2024-12-19 11:20:08 -0500

    Add project files.

---
+++ nuovo progetto/Source.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+#include <cstdlib>  // For srand() and rand()
+#include <ctime> 
+using namespace std;
+
+int generateRandomNumbers(int min, int max) {
+    // Seed the random number generator with the current time
+    //std::srand(static_cast<unsigned>(std::time(nullptr)));
+        int randomNumber = min + (std::rand() % (max - min + 1));
+		return randomNumber;
+}
+
+
+
+int main()
+{
+	//stack
+	stack<int> primostack;
+
+	int a = 5;
+
+	primostack.push(a);
+	primostack.pop();
+
+	for (int i = 0; i < 10; i++)
+	{
+		primostack.push(generateRandomNumbers(5, 100));
+	}
+
+	int contenitore;
+
+	int sizestack = primostack.size();
+
+	for (; !primostack.empty();)
+	{
+		contenitore = primostack.top();
+		primostack.pop();
+		cout << contenitore << endl;
+	}
+
+
+
+
+
+
+}
